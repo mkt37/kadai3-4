7fe61b166385a6fa66a0dbad0a4da56c5845693a
g15914km <g15914km@s30537.tsuda.ac.jp>
2017-05-11 16:53:35 +0900

    testクラスを作りました。

---
+++ snake-camel/test/junit/snake/SnakeTest.java
@@ -0,0 +1,14 @@
+package junit.snake;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeTest {
+
+	@Test
+	public void test() {
+		System.out.println("Not yet implemented");
+	}
+
+}
